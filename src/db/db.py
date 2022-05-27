a1d685b271d7e347e69827cc240d034a9a6bbb1d
Leonardo Martinez <shouta.aizawa0709@gmail.com>
2022-05-26 20:27:38 -0500

    update version

--- src/db/db.py
+++ src/db/db.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine, MetaData
 
 
 
-#engine = create_engine('mysql+pymysql://root:password@34.94.79.113:3306/passport')
-engine = create_engine('mysql+pymysql://root:password@127.0.0.1:3306/passport')
+engine = create_engine('mysql+pymysql://root:password@34.94.79.113:3306/passport')
+#engine = create_engine('mysql+pymysql://root:password@127.0.0.1:3306/passport')
 
 conn = engine.connect()
 
